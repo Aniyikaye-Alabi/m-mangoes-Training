852df6f24f6da498629b542b1c6c8eb9895771ae
Aniyikaye Alabi <adalabi@sidmach.com>
2022-10-20 09:30:35 +0100

    Remove ignored files

---
+++ Calculator/Calculate.cs
@@ -0,0 +1,27 @@
+﻿namespace GenerateReceipt.Calculator
+{
+    public class Calculate
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Division(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException();
+            }
+            return x / y;
+
+        }
+    }
+}
